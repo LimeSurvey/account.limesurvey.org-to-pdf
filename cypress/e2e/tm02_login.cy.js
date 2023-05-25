b62b2cadce50dfbec6b39e9710759b2f50f8ae92
tiborpacalat <129872163+tiborpacalat@users.noreply.github.com>
2023-05-25 17:29:23 +0200

    some cleanup

--- cypress/e2e/tm02_login.cy.js
+++ cypress/e2e/tm02_login.cy.js
@@ -1,12 +1,12 @@
 describe('TM02 Login', () => {
   it('TM02.1 Login email - username', function () {
-    cy.visit(`${Cypress.env('LANG')}`)
+    cy.visit(Cypress.env('LANG'))
     cy.wait(1000)
     cy.screenshot()
   })
 
   it('TM02.2 Forgot password', function () {
-    cy.visit(`${Cypress.env('LANG')}`)
+    cy.visit(Cypress.env('LANG'))
     cy.get('.link-register a').eq(0).click()
     cy.get('.fa-user-shield').should('be.visible')
     cy.wait(1000)
@@ -14,7 +14,7 @@ describe('TM02 Login', () => {
   })
 
   it('TM02.3 Forgot username', function () {
-    cy.visit(`${Cypress.env('LANG')}`)
+    cy.visit(Cypress.env('LANG'))
     cy.get('.link-register a').eq(1).click()
     cy.get('.fa-user-shield').should('be.visible')
     cy.wait(1000)
@@ -22,7 +22,7 @@ describe('TM02 Login', () => {
   })
 
   it('TM02.4 2 factor page', function () {
-    cy.visit(`${Cypress.env('LANG')}/`)
+    cy.visit(Cypress.env('LANG'))
     cy.login(Cypress.env('USER'), Cypress.env('PASS'))
     cy.get('#users-mfa-captive-button-submit').should('be.visible')
     cy.wait(1000)
@@ -30,7 +30,7 @@ describe('TM02 Login', () => {
   })
 
   it('TM02.5 Confirmation email sent', function () {
-    cy.visit(`${Cypress.env('LANG')}`)
+    cy.visit(Cypress.env('LANG'))
     cy.get('.link-register a').eq(0).click()
     cy.get('#jform_email').type('tester@testing.de')
     cy.get('button[type="submit"]').click()
@@ -40,7 +40,7 @@ describe('TM02 Login', () => {
   })
 
   it('TM02.6 select 2 factor auth method', function () {
-    cy.visit(`${Cypress.env('LANG')}/`)
+    cy.visit(Cypress.env('LANG'))
     cy.login(Cypress.env('USER'), Cypress.env('PASS'))
     cy.get('#users-mfa-captive-button-submit').should('be.visible')
     cy.get(
@@ -54,7 +54,7 @@ describe('TM02 Login', () => {
   })
 
   it('TM02.7 backup codes', function () {
-    cy.visit(`${Cypress.env('LANG')}/`)
+    cy.visit(Cypress.env('LANG'))
     cy.login(Cypress.env('USER'), Cypress.env('PASS'))
     cy.get('#users-mfa-captive-button-submit').should('be.visible')
     cy.get(
